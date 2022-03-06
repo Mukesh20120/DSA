ec3015c8199afa38dd55f8a1875fa612a31ceca0
laptop <mukeshsharma20120@gmail.com>
2022-03-06 13:43:06 +0530

    recursive solution

--- TREE/Add_one_row_to_tree.cpp
+++ TREE/Add_one_row_to_tree.cpp
@@ -40,3 +40,44 @@ public:
         return root;
     }
 };
+
+//2 method recursivly
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth==1){
+            TreeNode *temp=new TreeNode(val);
+            temp->left=root;
+            return temp;
+        }
+        Insert(val,root,depth,1);
+        return root;
+    }
+    void Insert(int v,TreeNode *node,int d,int n){
+        if(!node)return;
+        if(d==n-1){
+            TreeNode *l=new TreeNode(v);
+            TreeNode *r=new TreeNode(v);
+            l->left=node->left;
+            r->right=node->right;
+            node->left=l;
+            node->right=r;
+            return;
+        }
+        else{
+            Insert(v,node->left,d,n+1);
+            Insert(v,node->right,d,n+1);
+        }
+    }
+};
