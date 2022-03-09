6f735774176df9f108bc0178920777cd97c3ab99
Free_spirit <mukeshsharma20120@gmail.com>
2022-03-09 23:18:02 +0530

    just other way to do this question

--- TREE/maximum_difference_between_node_and_ancestor.cpp
+++ TREE/maximum_difference_between_node_and_ancestor.cpp
@@ -1,35 +1,58 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int helper(TreeNode *node,int curmax,int curmin){
+
+class Solution
+{
+  public:
+    int helper(TreeNode *node, int curmax, int curmin)
+    {
         //returning the difference b/w min and max
-        if(!node){
-            return curmax-curmin;
+        if (!node)
+        {
+            return curmax - curmin;
         }
         //update max value and min value
-        curmax=max(node->val,curmax);
-        curmin=min(node->val,curmin);
+        curmax = max(node->val, curmax);
+        curmin = min(node->val, curmin);
         //passing in left and right tree recursively
-        int left=helper(node->left,curmax,curmin);
-        int right=helper(node->right,curmax,curmin);
+        int left = helper(node->left, curmax, curmin);
+        int right = helper(node->right, curmax, curmin);
         //taking max difference
-        return max(left,right);
+        return max(left, right);
     }
-    
-    int maxAncestorDiff(TreeNode* root) {
-        return helper(root,-1,INT_MAX);
+
+    int maxAncestorDiff(TreeNode *root)
+    {
+        return helper(root, -1, INT_MAX);
     }
 };
 
+/**
+ nothing much change just declare a global variable to store the difference and 
+ each time updating it
+ */
+class Solution
+{
+  private:
+    int ans;
 
+  public:
+    void helper(TreeNode *node, int mx, int mn)
+    {
+        if (!node)
+            return;
+        mx = max(mx, node->val);
+        mn = min(mn, node->val);
+        ans = max(ans, abs(mx - mn));
 
+        helper(node->left, mx, mn);
+        helper(node->right, mx, mn);
+
+        return;
+    }
+    int maxAncestorDiff(TreeNode *root)
+    {
+        ans = 0;
+        int mn = INT_MAX;
+        helper(root, -1, mn);
+        return ans;
+    }
+};
