d55bc2d5c0274fe16889374f70a1528e0260b5ed
laptop <mukeshsharma20120@gmail.com>
2022-03-15 19:27:48 +0530

    string matching question naive approach recursion

---
+++ DP/distinct_subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+    int fun(string &s,string &t,int i,int j){
+        //base case if we at the end of t string means we match 
+        //all the character of t string from s
+        if(j<0)return 1;
+        //if j is still have character but we reach the end of the first string
+        if(i<0)return 0;
+        
+        //two choices
+        
+        //if we get the matching character
+        if(s[i]==t[j]){
+            //we move ahead on both string and we not taking the match string 
+            return fun(s,t,i-1,j-1)+fun(s,t,i-1,j);
+        }
+            return fun(s,t,i-1,j);//character not match we check next character
+    }
+
+    int numDistinct(string s, string t) {
+      int n=s.length();
+      int m=t.length();
+        return fun(s,t,n-1,m-1);
+    }
+};
