4aad187efd5bae5012ebf305635b0d493372c6af
laptop <mukeshsharma20120@gmail.com>
2022-03-24 19:50:53 +0530

    optimal approach of prims algorithms

--- GRAPH/minimum_spanning_tree.cpp
+++ GRAPH/minimum_spanning_tree.cpp
@@ -90,3 +90,76 @@ int main()
 }
 
   // } Driver Code Ends
+
+
+//2nd method prims algo optimized version using priority queue
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {     bool visit[V]={false};
+          vector<int>key(V,INT_MAX);
+          int ans=0;
+          priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+           pq.push({0,0});
+           key[0]=0;
+           while(!pq.empty()){
+               int u=pq.top().second;
+               int k=pq.top().first;
+               pq.pop();
+               visit[u]=true;
+           for(auto it: adj[u]){
+               int n=it[0];
+               int w=it[1];
+               if(visit[n]==false && w<key[n]){
+                   key[n]=w;
+                   pq.push({w,n});
+               }
+           }
+         }
+         for(int sum: key)
+            ans+=sum;
+       return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
