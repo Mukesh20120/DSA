bacedbf459c116ac2c25e5b1e7955d927db2a23a
laptop <mukeshsharma20120@gmail.com>
2022-03-25 06:50:27 +0530

    optimal approach

--- SLIDING_WINDOW/first_negative_integer_window_size.cpp
+++ SLIDING_WINDOW/first_negative_integer_window_size.cpp
@@ -1,33 +1,4 @@
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<long long> printFirstNegativeInteger(long long int arr[],
-                                             long long int n, long long int k);
-
-// Driver program to test above functions
-int main() {
-    long long int t, i;
-    cin >> t;
-    while (t--) {
-        long long int n;
-        cin >> n;
-        long long int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        long long int k;
-        cin >> k;
-
-        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
-        for (auto it : ans) cout << it << " ";
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
-
-
+// naive approach 
 
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
@@ -46,4 +17,25 @@ vector<long long> printFirstNegativeInteger(long long int A[],
         ans.push_back(0);
     }
     return ans;
+ }
+
+// 2 method optimal approach 
+
+
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+       vector<long long>ans;
+       int j=-1;
+       for(int i=0;i<=N-K;i++){
+           if(j<i){
+              j=i;
+           while(j<N && A[j]>0)j++;
+            }//our negative index
+         if(j>=i && j<i+K)
+           ans.push_back(A[j]);
+         else
+           ans.push_back(0);
+       }
+       return ans;
  }
