2a296361aa4a7f9298371f5b405b1353065bb5ba
laptop <mukeshsharma20120@gmail.com>
2022-03-27 22:38:15 +0530

    little bit modification in code

--- BIT/count_no_of_subarry_max_or.cpp
+++ BIT/count_no_of_subarry_max_or.cpp
@@ -21,3 +21,25 @@ public:
         return ans+knapsack(nums,sum,n-1,cur)+knapsack(nums,sum,n-1,cur|nums[n]);
     }
 };
+
+//little bit of changes 
+class Solution {
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        //step 1 find the max or of nums array
+        //as or of 3 and 4 greater mean all value give max 
+        int maxi=0;
+        for(int &it: nums)
+            maxi=maxi|it;
+        //step 2 question boil down to knapsack find 
+        //all the subarray have sum =7
+        return knapsack(nums,maxi,nums.size()-1,0);
+    }
+    int knapsack(vector<int>&nums,int sum,int n,int cur){
+      //  int ans=0;
+        if(n<0)
+            return sum==cur;
+
+        return knapsack(nums,sum,n-1,cur)+knapsack(nums,sum,n-1,cur|nums[n]);
+    }
+};
