9e0582e5ed0a0d5321308cab09f602f4ef3025d2
laptop <mukeshsharma20120@gmail.com>
2022-04-07 23:44:18 +0530

    2d optimal solution then 1d

--- DP/longest_increasing_subsequence.cpp
+++ DP/longest_increasing_subsequence.cpp
@@ -38,3 +38,42 @@ public:
         return helper(-1,nums,n,0,dp);
     }
 };
+
+//tabular form 
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int len=dp[i+1][j+1];
+                if(j==-1 || nums[i]>nums[j]){
+                    len=max(len,1+dp[i+1][i+1]);
+                }
+                dp[i][j+1]=len;
+            }
+        }
+        return dp[0][0];
+    }
+};
+//space optimized above code using prev and cur 1d vector
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>prev(n+1,0);
+        vector<int>cur(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int len=prev[j+1];
+                if(j==-1 || nums[i]>nums[j]){
+                    len=max(len,1+prev[i+1]);
+                }
+                cur[j+1]=len;
+            }
+            swap(cur,prev);
+        }
+        return prev[0];
+    }
+};
