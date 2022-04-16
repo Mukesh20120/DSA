3cdb8a5483393dc0b20a257c6ff40ddab9ed1424
laptop <mukeshsharma20120@gmail.com>
2022-04-16 18:28:45 +0530

    brute force approach linear and optimized by binary search

---
+++ BINARY_SEARCH/first_and_last_position_of_element_sorted_array.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //brute force approach 
+        //we find first occarance by linear search and then then continuation
+        vector<int>ans={-1,-1};
+        int n=nums.size();
+        if(n==0)return ans;
+        //finding first occarance
+        int i,j;
+        for(i=0;i<n;i++)
+            if(nums[i]==target)
+                break;
+        if(i==n){
+            return ans;
+        }
+        ans[0]=i;
+        //last occarance
+        for(j=i;j<n;j++)
+            if(nums[j]!=target)
+                break;
+        ans[1]=--j;
+        
+        return ans;
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //optimal approach using binary search
+        int low=0,n=nums.size(),high=n-1;
+        vector<int>ans={-1,-1};
+        //first occerance check left side
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                ans[0]=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+         //last occerance check left side
+        //reset low and  high again
+        low=0;
+        high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                ans[1]=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return ans;
+    }
+};
