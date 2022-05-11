967b6ef90af9622155e5c70e1cd93b3a49410647
laptop <mukeshsharma20120@gmail.com>
2022-05-11 22:02:05 +0530

    optimal solution using priority queue

--- QUEUE/find_median_from_data_stream.cpp
+++ QUEUE/find_median_from_data_stream.cpp
@@ -21,4 +21,49 @@ public:
     }
 };
 
+
+//optimal solution using priority queue
+class MedianFinder {
+    private:
+    //define max heap 
+    priority_queue<int>maxheap;
+    //definde min heap
+    priority_queue<int,vector<int>,greater<int>>minheap;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        //first we push in max heap
+        maxheap.push(num);
+        //pushing the top of max heap in min heap
+        minheap.push(maxheap.top());
+        //pop back from max heap
+        maxheap.pop();
+        //checking if the size of min heap is greater than max heap
+        if(minheap.size()>maxheap.size()){
+            //taking out from min
+            maxheap.push(minheap.top());
+            //empty the top element from the min 
+            minheap.pop();
+        }
+        
+    }
+    
+    double findMedian() {
+        //now we check the test case if the max heap is greater than min then we return the maxheaptop
+        if(maxheap.size()>minheap.size())
+            return maxheap.top();
+        return (minheap.top()+maxheap.top())/2.0;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+
  
