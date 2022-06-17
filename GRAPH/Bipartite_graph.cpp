e1f719e056186fb505ca9a8b0b7e7c9a28a9b00c
laptop <mukeshsharma20120@gmail.com>
2022-06-17 18:05:49 +0530

    revision level ordere traversla

---
+++ GRAPH/Bipartite_graph.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+   bool bfsbipartite(vector<int>adj[],int node,vector<int>&color){
+       queue<int>q; 
+       q.push(node); //pushing first node in queue
+       color[node]=1; //marking as color and also visited
+       while(!q.empty()){ //bfs traversal
+           int x=q.front();
+           q.pop();
+           //traversal all its connected node
+           for(auto &it: adj[x]){
+               //we have two case if color is -1 other
+               //we have check adjacent node color
+               if(color[it]==-1){ //checking visited also
+                   color[it]=1-color[x]; //marking different color
+                   q.push(it); //push it in queue
+               }
+               else if(color[it]==color[x])//other case if it already color check if it is same or not
+                 return false;
+           }
+       }
+       return true;
+   }
+
+	bool isBipartite(int V, vector<int>adj[]){
+	    vector<int>color(V,-1);//it work as both visited as well as for color also
+	   // if we are given differnt graph of node
+	    for(int i=0;i<V;i++){
+	       if(color[i]==-1)//check as visited
+	        if(!bfsbipartite(adj,i,color))//starting the graph
+	          return false;
+	    }
+	    return true;
+	}
+
+};
---
+++ TREE/level_order_traversal.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* node)
+    { vector<int>ans;
+      if(node==NULL)return ans;
+      queue<Node*>q;
+      q.push(node);
+      while(!q.empty()){
+          Node *temp=q.front();
+          q.pop();
+          ans.push_back(temp->data);
+          if(temp->left)
+          q.push(temp->left);
+          if(temp->right)
+          q.push(temp->right);
+      }
+      return ans;
+    }
+};
