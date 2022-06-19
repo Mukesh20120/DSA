d8309f7680c17d5a2818864b69b5cf0a5bff31d1
laptop <mukeshsharma20120@gmail.com>
2022-06-19 23:24:21 +0530

    trie data structure

--- TRIE/search_suggestions_system.cpp
+++ TRIE/search_suggestions_system.cpp
@@ -24,3 +24,80 @@ public:
         return ans;
     }
 };
+
+
+//other solution time and space complexity is greater but the way question is solve is using trie data structure
+
+//making trie data structure
+class trie{
+    public:
+        trie *list[26];
+        bool flag;
+    trie(){
+       memset(list,0,sizeof(list));
+        flag=false;
+    }
+};
+class Solution {
+public:
+    void Insert(trie *node,vector<string>&pro){
+        int n=pro.size();
+        for(int i=0;i<n;i++){
+            trie *cur=node;
+            string s=pro[i];
+            for(int j=0;j<s.length();j++){
+                if(cur->list[s[j]-'a']==NULL)
+                    cur->list[s[j]-'a']=new trie();
+               cur=cur->list[s[j]-'a'];
+            }
+            cur->flag=true;
+        }
+    }
+    
+    void dfs(trie *cur,string word,vector<string>&result){
+        //we required only 3 size
+        if(result.size()==3)return;
+        //check it is a word or not
+        if(cur->flag)
+            result.push_back(word);
+        //running dfs
+        for(char c='a';c<='z';c++){
+          if(cur->list[c-'a']){
+              word+=c;
+              dfs(cur->list[c-'a'],word,result);
+              word.pop_back();
+          }
+        }
+    }
+    
+    
+    vector<string> getword(trie *root,string pre){
+        //move the pointer to the end of the pre string
+        trie *cur=root;
+        vector<string>result;
+        for(int i=0;i<pre.length();i++){
+            if(cur->list[pre[i]-'a']==NULL)
+                return result;
+           cur=cur->list[pre[i]-'a'];
+        }
+        //filling the result vector with search words
+        dfs(cur,pre,result);
+        
+        return result;
+    }
+    
+    vector<vector<string>> suggestedProducts(vector<string>& products, string sw) {
+        trie *root=new trie();
+        //now let insert all the element in trie
+        Insert(root,products);
+        //we store answer in 
+        vector<vector<string>>ans;
+        
+        string prefix;
+        for(char &c: sw){
+            prefix+=c;
+            ans.push_back(getword(root,prefix));
+        }
+        return ans;
+    }
+};
